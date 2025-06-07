623ba634a8df8eac14035f37301b1697113eee91
imammufiid <imufiid98@gmail.com>
2025-06-08 06:40:41 +0700

    feat: Updated app icon login

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import {CircleStackIcon} from '@heroicons/react/24/outline';
 import {useGoogleAuth} from "../../hooks/firebase/useGoogleAuth.ts";
 import {useNavigate} from "react-router-dom";
+import appIcon from '../../assets/appicon.png'
 import {USER_KEY} from "../../services/StorageService.ts";
 
 const LoginWithGoogle: React.FC = () => {
@@ -21,9 +22,7 @@ const LoginWithGoogle: React.FC = () => {
   return (
     <div className="flex items-center justify-center">
       <div className="flex flex-col items-center p-6  rounded-lg">
-        <CircleStackIcon
-          className="w-32 h-32 mb-6"
-        />
+        <img src={appIcon} className="w-44 h-44 mb-4" alt="logo"/>
         <button
           onClick={handleGoogleLogin}
           className="flex items-center px-4 py-2 text-white bg-red-500 rounded-lg hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400">
